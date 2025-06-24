5aa18c0773994e475c0d7bdc81a03d6afec331e3
Jake Sarjeant <jake@sarjeant.me>
2025-06-24 21:17:51 +0200

    chore: fix basic parsing routine

--- willi/src/lib.rs
+++ willi/src/lib.rs
@@ -3,18 +3,19 @@ use std::{io::Cursor, str::FromStr};
 use csv::{Position, ReaderBuilder, StringRecord};
 use serde::Deserialize;
 use thiserror::Error;
+use tracing::{debug, error};
 use wasm_bindgen::prelude::*;
 
 #[derive(Debug, Error)]
 pub enum ParseError {
   #[error("Ungültige Kopfzeile — Keine Versionsdaten verfügbar.")]
   InvalidHeader,
-  #[error("Lesen Abgebrochen — möglicherweise ist die Datei ungültig Kodiert")]
+  #[error("Lesen Abgebrochen — möglicherweise ist die Datei ungültig kodiert")]
   Aborted,
   #[error("Formatfehler — Zeile ohne Datentyp")]
   MissingType,
   #[error("Ungültige Datenzeile — {0}")]
-  Deserialize(csv::Error),
+  BadLine(csv::Error),
 }
 
 #[wasm_bindgen]
@@ -27,7 +28,7 @@ pub struct WilliStundenplan {
 }
 
 impl WilliStundenplan {
-  pub fn parse(source: &str) -> (WilliStundenplan, Vec<(Position, ParseError)>) {
+  pub fn parse(source: &str) -> (WilliStundenplan, Vec<(Option<Position>, ParseError)>) {
     let mut errors = vec![];
 
     let (raw_header, body) = source
@@ -36,7 +37,7 @@ impl WilliStundenplan {
       .unwrap_or((None, source));
 
     let header = raw_header.map(str::parse).transpose().unwrap_or_else(|e| {
-      errors.push((Position::new(), e));
+      errors.push((Some(Position::new()), e));
       None
     });
 
@@ -50,22 +51,23 @@ impl WilliStundenplan {
       .flexible(true)
       .from_reader(Cursor::new(body));
 
-    let mut peek_record = StringRecord::new();
-    while !csv_reader.is_done() {
-      // Peek record to determine prefix
-      let Ok(true) = csv_reader.read_record(&mut peek_record) else {
-        errors.push((csv_reader.position().clone(), ParseError::Aborted));
-        break;
+    for result in csv_reader.records() {
+      let record = match result {
+        Ok(rec) => rec,
+        Err(err) => {
+          errors.push((err.position().cloned(), ParseError::BadLine(err)));
+          continue;
+        }
       };
 
       // Extract prefix letters and ID.
       // This parses the first field, i.e. "LC12" => ("LC", 12)
       let (typ, id): (String, usize) = {
         // TODO: Correctly handle "TRnnzz" lines (double primary key)
-        let type_col = match peek_record.get(0) {
+        let type_col = match record.get(0) {
           Some(c) => c,
           None => {
-            errors.push((csv_reader.position().clone(), ParseError::MissingType));
+            errors.push((record.position().cloned(), ParseError::MissingType));
             continue;
           }
         };
@@ -80,20 +82,25 @@ impl WilliStundenplan {
           .unwrap_or((type_col.into(), 0))
       };
 
-      let record = match peek_record.deserialize(None) {
-        Ok(record) => record,
-        Err(err) => {
-          errors.push((
-            err.position().unwrap_or(csv_reader.position()).clone(),
-            ParseError::Deserialize(err),
-          ));
-          continue;
-        }
-      };
+      // So that each invocation can have a different target type. Needs to be a macro due to continue.
+      macro_rules! deserialize {
+        ($rec:ident) => {
+          match $rec.deserialize(None) {
+            Ok(record) => record,
+            Err(err) => {
+              errors.push((
+                err.position().or(record.position()).cloned(),
+                ParseError::BadLine(err),
+              ));
+              continue;
+            }
+          }
+        };
+      }
 
       match (&typ[..], id) {
-        ("W", _) => plan.schuldaten = Some(record),
-        _ => todo!(),
+        ("W", _) => plan.schuldaten = Some(deserialize!(record)),
+        _ => {}
       }
     }
 
@@ -101,40 +108,50 @@ impl WilliStundenplan {
   }
 }
 
+#[wasm_bindgen]
+impl WilliStundenplan {
+  #[wasm_bindgen(getter)]
+  pub fn willi_version(&self) -> Option<usize> {
+    self.header.as_ref().map(|h| h.version)
+  }
+}
+
 #[derive(Clone, Debug)]
 #[wasm_bindgen]
 #[wasm_bindgen(js_name = WilliParseError)]
 pub struct WasmError {
-  pub byte: u64,
-  pub line: u64,
-  pub record: u64,
+  pub byte: Option<u64>,
+  pub line: Option<u64>,
+  pub record: Option<u64>,
   #[wasm_bindgen(getter_with_clone)]
   pub error: String,
 }
 
 #[wasm_bindgen]
-pub struct ParseResult(
-  #[wasm_bindgen(getter_with_clone)] pub WilliStundenplan,
-  #[wasm_bindgen(getter_with_clone)] pub Vec<WasmError>,
-);
+pub struct ParseResult {
+  #[wasm_bindgen(getter_with_clone)]
+  pub plan: WilliStundenplan,
+  #[wasm_bindgen(getter_with_clone)]
+  pub errors: Vec<WasmError>,
+}
 
 #[cfg(target_family = "wasm")]
 #[wasm_bindgen(js_name = "parse_plan")]
 pub fn wasm_parse_plan(source: String) -> ParseResult {
   let (plan, errors) = WilliStundenplan::parse(&source);
 
-  ParseResult(
+  ParseResult {
     plan,
-    errors
+    errors: errors
       .iter()
       .map(|(p, error)| WasmError {
-        byte: p.byte(),
-        line: p.line(),
-        record: p.record(),
+        byte: p.as_ref().map(Position::byte),
+        line: p.as_ref().map(Position::line),
+        record: p.as_ref().map(Position::record),
         error: format!("{}", error),
       })
       .collect(),
-  )
+  }
 }
 
 #[derive(Debug, Clone)]
@@ -168,7 +185,22 @@ impl FromStr for WilliHeader {
 #[derive(Debug, Clone, Deserialize)]
 pub struct SchuldatenZeile {
   pub schulname: String,
+  #[serde(default)]
   pub titel1: Option<String>,
+  #[serde(default)]
   pub titel2: Option<String>,
+  #[serde(default)]
   pub schulnummer: Option<usize>,
+}
+
+#[wasm_bindgen(start)]
+pub fn start() -> Result<(), JsValue> {
+  // print pretty errors in wasm https://github.com/rustwasm/console_error_panic_hook
+  // This is not needed for tracing_wasm to work, but it is a common tool for getting proper error line numbers for panics.
+  console_error_panic_hook::set_once();
+
+  // Add this line:
+  wasm_tracing::set_as_global_default();
+
+  Ok(())
 }
